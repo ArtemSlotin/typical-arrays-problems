03855422caed6eb1d0af1632587f8dc5f724e5f9
Artem Slotin <66305683+ArtemSlotin@users.noreply.github.com>
2021-03-15 16:33:51 +0200

    Done Typical Array Problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array instanceof Array) {
+        if (array.length > 0) {
+            array.sort((a, b) => {return a - b});
+            console.log(array);
+            return array[0];
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array instanceof Array) {
+        if (array.length > 0) {
+            array.sort((a, b) => {return a - b});
+            console.log(array);
+            return array[array.length - 1];
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+    
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array instanceof Array) {
+        if (array.length > 0) {
+            let average = 0;
+            for(let i = 0; i < array.length; i++) {
+             average += array[i];
+             console.log(array[i]);
+            }
+             return average / array.length;
+         } else {
+             return 0;
+         }
+    } else {
+        return 0;
+    }   
 }
